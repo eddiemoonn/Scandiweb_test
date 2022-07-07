b8f6d9992c4aa8df116321c21fc3ab4e7c4282aa
eddiemoonn <benny@Anihs-MacBook-Pro.local>
2022-07-07 20:52:04 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import $, { event } from "jquery";
+import "./App.css";
 
 function App() {
+  
+  const [result, setResult] = useState("");
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    const form = $(e.target);
+    $.ajax({
+      type: "POST",
+      url: form.attr("action"),
+      data: form.serialize(),
+      success(data) {
+        setResult(data);
+      },
+    });
+  };
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const form = $(e.target);
+    $.ajax({
+      type: "POST",
+      url: form.attr("action"),
+      data: form.serialize(),
+      success(data) {
+        setResult(data);
+      },
+    });
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form
+        action="http://localhost:8000/server.php"
+        method="post"
+        onSubmit={(event) => {this.handleDelete(event); this.handleAdd(event)}}
+      >
+        <h2 htmlFor="name">Product List </h2>
+        <div className="btn-container">
+          <button className="Add">ADD</button>
+          <button className="Delete">MASS DELETE</button>
+        </div>
+        
+      </form>
+      <h1>{result}</h1>
     </div>
   );
 }
