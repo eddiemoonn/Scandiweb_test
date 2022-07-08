8ce2fcfc1f857088904b17e30e734440857646b1
eddiemoonn <benny@Anihs-MacBook-Pro.local>
2022-07-08 10:19:35 +0100

    updated css

--- src/App.js
+++ src/App.js
@@ -1,53 +1,47 @@
 import { useState } from "react";
 import $, { event } from "jquery";
 import "./App.css";
+import Products from "./experimental/mockData"
 
 function App() {
   
   const [result, setResult] = useState("");
 
   const handleDelete = (e) => {
-    e.preventDefault();
-    const form = $(e.target);
-    $.ajax({
-      type: "POST",
-      url: form.attr("action"),
-      data: form.serialize(),
-      success(data) {
-        setResult(data);
-      },
-    });
+    
   };
 
   const handleAdd = (e) => {
-    e.preventDefault();
-    const form = $(e.target);
-    $.ajax({
-      type: "POST",
-      url: form.attr("action"),
-      data: form.serialize(),
-      success(data) {
-        setResult(data);
-      },
-    });
+    
   };
 
 
   return (
     <div className="App">
-      <form
-        action="http://localhost:8000/server.php"
-        method="post"
-        onSubmit={(event) => {this.handleDelete(event); this.handleAdd(event)}}
-      >
-        <h2 htmlFor="name">Product List </h2>
-        <div className="btn-container">
-          <button className="Add">ADD</button>
-          <button className="Delete">MASS DELETE</button>
+   <div className="header-container">
+    <h2 htmlFor="name">Product List </h2>
+      <div className="btn-container">
+        <button className="Add">ADD</button>
+        <button className="Delete">MASS DELETE</button>
+      </div>
+   </div>
+      <section className="products-section">
+        <div className="products-container">
+        {Products.map(product => {
+          return (
+            <div className="product-item" key={product.producId}>
+              <input type = "checkbox"/>
+              <h3>{product.productName}</h3>
+              <p>{product.price}</p>
+
+            </div>
+          )
+         
+        })}
         </div>
         
-      </form>
-      <h1>{result}</h1>
+
+      </section>
     </div>
   );
 }
---
+++ src/experimental/mockData.js
@@ -0,0 +1,54 @@
+
+   const Products = [
+        {
+            producId: "JVC1",
+            productName: "Acme DISC",
+            price: "$ 50"
+
+        },
+        {
+            producId: "JVC2",
+            productName: "Acme DISC",
+            price: "$ 50"
+
+        },
+        {
+            producId: "JVC3",
+            productName: "Acme DISC",
+            price: "$ 50"
+
+        },
+        {
+            producId: "JVC4",
+            productName: "Acme DISC",
+            price: "$ 50"
+
+        },
+        {
+            producId: "JVC5",
+            productName: "Acme DISC",
+            price: "$ 50"
+
+        },
+        {
+            producId: "JVC6",
+            productName: "Acme DISC",
+            price: "$ 50"
+
+        },
+        {
+            producId: "JVC7",
+            productName: "Acme DISC",
+            price: "$ 50"
+
+        },
+        {
+            producId: "JVC8",
+            productName: "Acme DISC",
+            price: "$ 50"
+
+        },
+
+    ]
+
+    export default Products;
